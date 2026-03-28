93e4ca1a1b8f79b7e99ec5298376f360f5a5a99f
Benny <asshimbenny01@gmail.com>
2026-03-28 09:46:06 +0300

    Milkshaeke equal to The javashake

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function addBurger(){
     burgers.push(newBurger);
 }
 
-if(false === true){
+if(true === true){
     const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger);
 }
