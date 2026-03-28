4ae0680f6859d03ad469f7af666d4d25c138fa0c
Benny <asshimbenny01@gmail.com>
2026-03-28 09:44:56 +0300

    Milkshaeke equal to The javashake

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Determine the types of scope you will need to create for the code structure. 
 // You will be developing a menu with milkshakes and burgers. 
 // You will also be creating menu features for both the milkshake and burger.
-
+//index.js if statement uses the .push() array method to add anotherNewBurger, which has the value of 'Maple Bacon Burger', to the burgers array
+//Expected the burgers array to have the string 'Maple Bacon Burger' as one of its elements: expected false to equal true
 const burgers = ['Hamburger', 'Cheeseburger'];
 let featuredDrink = 'Strawberry Milkshake';
 JavaShake = 'Strawberry Milkshake';
@@ -11,10 +12,11 @@ function addBurger(){
     burgers.push(newBurger);
 }
 
-if(true){
+if(false === true){
     const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger);
 }
+
 changeFeaturedDrink();
 function changeFeaturedDrink(){
     featuredDrink = 'Strawberry Milkshake';
